1cf1cbff6200ce4cd301fd0a4337e174fe9d8b3e
Konstantin <ivaninkv@outlook.com>
2018-10-04 21:36:41 +0300

    file.py complete
    Завершена работа с файлом file.py

--- 1. Diving in Python/Week 4/file.py
+++ 1. Diving in Python/Week 4/file.py
@@ -2,6 +2,8 @@ import os
 import tempfile
 
 class File:
+    seek = 0
+
     def __init__(self, full_filepath):
         self.full_filepath = full_filepath
 
@@ -12,15 +14,23 @@ class File:
         return self
 
     def __next__(self):
-        # readlines
-        raise StopIteration
+        with open(self.full_filepath) as f:
+            f.seek(self.seek)
+            result = f.readline()
+            self.seek = f.tell()
+            if result == '':
+                raise StopIteration
+            return result
 
     def __add__(self, second_file):
-        new_path = os.path.join(tempfile.gettempdir, 'new_file.txt')
-        with open(new_path) as f:            
-            pass
-            #for line in self:
-            #    f.write(line)
+        new_path = os.path.join(tempfile.gettempdir(), 'new_file.txt')
+        with open(new_path, 'w') as f:            
+            for line in self:
+                f.write(line)
+            for line in second_file:
+                f.write(line)  
+            f.write('\n')
+        return File(new_path)
 
 
     def write(self, text):
@@ -29,9 +39,17 @@ class File:
 
 
 def main():
-    f = File('test.txt')   
-    f.write('tost\n')
-    print(f) 
+    f = File('test.txt')
+    print(f.write('a\na\n'))
+    f2 = File('test2.txt')
+    f2.write('b\nc')
+    f3 = f + f2
+    f.seek = 1
+    f2.seek = 2
+    print(f.seek, f2.seek)
+
+    for line in f3:
+        print(line, end='')
 
 if __name__ == '__main__':
     main()
